d7f3ab7074fc7ffea521ef9304445b97c38e8e4f
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-11-20 16:23:12 +0600

    Spring. Область видимости.

--- src/main/java/spring/main/Start.java
+++ src/main/java/spring/main/Start.java
@@ -7,14 +7,25 @@ import spring.impls.robot.ModelT1000;
 public class Start {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-        System.out.println("=============== Object1 ===================");
+        /*System.out.println("=============== Object1 =====================");
+        System.out.println("=============== Singleton ===================");
         ModelT1000 t10001 = (ModelT1000)context.getBean("t10001");
-        t10001.action();
-        System.out.println("=============== Object2 ===================");
+        System.out.println(t10001);
+        t10001 = (ModelT1000)context.getBean("t10001");
+        System.out.println(t10001);
+        System.out.println();
+        //t10001.action();
+        System.out.println("=============== Object2 =====================");
+        System.out.println("=============== Prototype ===================");
         ModelT1000 t10002 = (ModelT1000)context.getBean("t10002");
-        t10002.action();
+        System.out.println(t10002);
+        t10002 = (ModelT1000)context.getBean("t10002");
+        System.out.println(t10002);
+
+        /*
         System.out.println("=============== Object3 ===================");
         ModelT1000 t10003 = (ModelT1000)context.getBean("t10003");
         t10003.action();
+        */
     }
 }
