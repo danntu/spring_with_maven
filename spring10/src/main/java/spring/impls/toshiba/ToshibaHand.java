3180ac356be5ff44ebeb21e2f85747122191e80d
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-22 17:13:57 +0600

    Область  видимости Bean Scope

---
+++ spring10/src/main/java/spring/impls/toshiba/ToshibaHand.java
@@ -0,0 +1,9 @@
+package spring.impls.toshiba;
+
+import spring.interfaces.Hand;
+
+public class ToshibaHand implements Hand{
+    public void catchSomething() {
+        System.out.println("Catch Toshiba");
+    }
+}
---
+++ spring10/src/main/java/spring/impls/toshiba/ToshibaLeg.java
@@ -0,0 +1,9 @@
+package spring.impls.toshiba;
+
+import spring.interfaces.Leg;
+
+public class ToshibaLeg implements Leg{
+    public void go() {
+        System.out.println("Go Toshiba");
+    }
+}
