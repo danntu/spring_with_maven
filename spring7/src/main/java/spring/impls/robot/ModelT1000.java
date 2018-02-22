d715937a844a366a85b8817f2da0d1ac7c84b231
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-22 12:59:01 +0600

    Using Getter and Setter

---
+++ spring7/src/main/java/spring/impls/robot/ModelT1000.java
@@ -0,0 +1,66 @@
+package spring.impls.robot;
+
+import spring.impls.sony.SonyHand;
+import spring.impls.sony.SonyHead;
+import spring.impls.toshiba.ToshibaLeg;
+import spring.interfaces.Hand;
+import spring.interfaces.Head;
+import spring.interfaces.Leg;
+import spring.interfaces.Robot;
+
+public class ModelT1000 implements Robot {
+
+    private Hand hand;
+    private Leg leg;
+    private Head head;
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public Leg getLeg() {
+        return leg;
+    }
+
+    public void setLeg(Leg leg) {
+        this.leg = leg;
+    }
+
+    public Head getHead() {
+        return head;
+    }
+
+    public void setHead(Head head) {
+        this.head = head;
+    }
+
+    public ModelT1000() {
+//        hand = new SonyHand();
+//        leg = new ToshibaLeg();
+//        head = new SonyHead();
+    }
+
+    public ModelT1000(Hand hand, Leg leg, Head head) {
+        super();
+        this.hand = hand;
+        this.leg = leg;
+        this.head = head;
+    }
+
+
+    public void fire() {
+        head.calc();
+        hand.catchSomething();
+        leg.go();
+    }
+
+
+    public void dance() {
+        System.out.println("T1000 танцует!");
+    }
+
+}
---
+++ spring7/src/main/java/spring/main/Start.java
@@ -0,0 +1,28 @@
+package spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring.impls.robot.ModelT1000;
+
+public class Start {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		System.out.println();
+		System.out.println("default");
+		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
+		t1000.dance();
+		t1000.fire();
+
+		System.out.println("");
+		System.out.println("Bean for Sony");
+		ModelT1000 t1000Sony = (ModelT1000) context.getBean("t1000Sony");
+		t1000Sony.dance();
+		t1000Sony.fire();
+
+		System.out.println("");
+		System.out.println("Bean for Toshiba");
+		ModelT1000 t1000Toshiba = (ModelT1000) context.getBean("t1000Toshiba");
+		t1000Toshiba.dance();
+		t1000Toshiba.fire();
+	}
+}
